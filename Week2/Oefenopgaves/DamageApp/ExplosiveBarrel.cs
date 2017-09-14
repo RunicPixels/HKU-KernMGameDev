0e054f19319f8b14d14d26f530ffed629cbac5be
Cheezegami <superronald@live.nl>
2017-09-14 11:45:34 +0200

    Many Changes, Wow.

---
+++ Week2/Oefenopgaves/DamageApp/ExplosiveBarrel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamageApp {
+    class ExplosiveBarrel : Entity {
+        public override void Die() {
+            base.Die();
+        }
+    }
+}
---
+++ Week2/Oefenopgaves/DamageApp/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DamageApp {
+    class Player {
+        Gun gun;
+
+        public virtual void HandleWeapons() {
+
+        }
+    }
+}
