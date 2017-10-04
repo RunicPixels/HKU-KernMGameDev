74206e64c8ccfbd9d694023a8d947fed436e3949
Cheezegami <superronald@live.nl>
2017-10-04 18:36:07 +0200

    new folder

---
+++ FiniteStateMachine/FSM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiniteStateMachine {
+    class FSM {
+        State currentState;
+        
+        void SetState(State menuState) {
+
+        }
+
+        void Update() {
+
+        }
+
+    }
+}
---
+++ Les4Werk/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les4Werk {
+    public static class Program {
+        private static Dictionary<string, Dictionary<GameObject, Action>> dictionary
+
+        static void Main( string[] args ) {
+        }
+    }
+}
